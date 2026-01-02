f69b0f11c780bcfff4d80aaf0c968e9db74cc19f
chunza2542 <hello@thechun.dev>
2026-01-02 09:58:46 +0700

    Typo

--- pumpkin/4x_drone.py
+++ pumpkin/4x_drone.py
@@ -6,10 +6,6 @@ size = get_world_size()
 utils.goto(0, 0)
 # utils.clean_the_land()
 
-drone_tasks = []
-total_drone = 4
-
-
 while True:
 	plant_start_i = 0
 	plant_end_i = 0
@@ -21,9 +17,9 @@ while True:
 		while i <= plant_end_i and j < size:
 			plant(Entities.Pumpkin)
 			use_item(Items.Water)
+
 			j += 1
 			move(North)
-
 			if j % size == 0:
 				i += 1
 				j = 0
@@ -51,15 +47,18 @@ while True:
 	# first half
 	plant_start_i = 0
 	plant_end_i = size // 2
-	spawn_drone(plant_the_pumpkin)
+	a = spawn_drone(plant_the_pumpkin)
 	do_a_flip()
-	fix = spawn_drone(fix_bad_yield)
+	b = spawn_drone(fix_bad_yield)
 
 	# second half
 	plant_start_i = size // 2 + 1
 	plant_end_i = size - 1
-	spawn_drone(plant_the_pumpkin)
+	c = spawn_drone(plant_the_pumpkin)
 	do_a_flip()
 	fix_bad_yield()
+	wait_for(a)
+	wait_for(b)
+	wait_for(c)
 	
 	harvest()
