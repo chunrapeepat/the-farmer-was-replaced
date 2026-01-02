b24cab90618785d3e2a259e4c4d4d1beaaaf9783
chunza2542 <hello@thechun.dev>
2026-01-02 19:59:33 +0700

    Sort both ways

--- cactus.py/4x_drone.py
+++ cactus.py/4x_drone.py
@@ -1,41 +1,108 @@
 import utils
 change_hat(Hats.Cactus_Hat)
 
-total_drone=4
+total_drone=3
 size = get_world_size()
 utils.goto(0, 0)
-utils.clean_the_land(entity=Entities.Cactus)
+utils.clean_the_land(Grounds.Soil, 4, Entities.Cactus)
 
 while True:
-    row = 0
-    def bubble_sort():
-			arr = []
-			j = 0
-			utils.goto(row, 0)
-			while j < size:
-				arr.append(measure())
-				move(North)
-				j += 1
-			# now you're at the start of the row
-			j = 0
-			while j < size - 1:
-				if arr[j] > arr[j+1]:
-					swap(North)
-					# swap actual arr
-					tmp = arr[j+1]
-					arr[j+1] = arr[j]
-					arr[j] = tmp
-					# move back 1 step
-					if j == 0:
-						move(North)
-						j += 1
-						continue
-					else:
-						j -= 1
-						move(South)
-				else:
+	row = 0
+	def bubble_sort():
+		arr = []
+		j = 0
+		utils.goto(row, 0)
+		while j < size:
+			arr.append(measure())
+			move(North)
+			j += 1
+		# now you're at the start of the row
+		j = 0
+		while j < size - 1:
+			if arr[j] > arr[j+1]:
+				swap(North)
+				# swap actual arr
+				tmp = arr[j+1]
+				arr[j+1] = arr[j]
+				arr[j] = tmp
+				# move back 1 step
+				if j == 0:
 					move(North)
 					j += 1
+					continue
+				else:
+					j -= 1
+					move(South)
+			else:
+				move(North)
+				j += 1
 
-    bubble_sort()
-    break
+	# sort vertically
+	drone_used = 0
+	tasks = []
+	for r in range(size):
+		row = r
+		t = spawn_drone(bubble_sort)
+		tasks.append(t)
+		drone_used += 1
+
+		if (drone_used == total_drone):
+			while len(tasks) > 0:
+				t = tasks.pop()
+				drone_used -= 1
+				if t == None:
+					continue
+				wait_for(t)
+		
+	for index in tasks:
+		if tasks[index] != None:
+			wait_for(tasks[index])
+
+	col = 0
+	def bubble_sort_horizontally():
+		arr = []
+		j = 0
+		utils.goto(0, col)
+		while j < size:
+			arr.append(measure())
+			move(East)
+			j += 1
+		# now you're at the start of the col
+		j = 0
+		while j < size - 1:
+			if arr[j] > arr[j+1]:
+				swap(East)
+				# swap actual arr
+				tmp = arr[j+1]
+				arr[j+1] = arr[j]
+				arr[j] = tmp
+				# move back 1 step
+				if j == 0:
+					move(East)
+					j += 1
+					continue
+				else:
+					j -= 1
+					move(West)
+			else:
+				move(East)
+				j += 1
+	
+	drone_used = 0
+	tasks = []
+	for c in range(size):
+		col = c
+		t = spawn_drone(bubble_sort_horizontally)
+		tasks.append(t)
+		drone_used += 1
+
+		if (drone_used == total_drone):
+			while len(tasks) > 0:
+				t = tasks.pop()
+				drone_used -= 1
+				if t == None:
+					continue
+				wait_for(t)
+
+	harvest()
+	utils.clean_the_land(Grounds.Soil, 4, Entities.Cactus)
